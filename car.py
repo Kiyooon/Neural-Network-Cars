b49b716ce4a85fa07cbfc7108df342ae86f9263e
Kiyooon <97262272+Kiyooon@users.noreply.github.com>
2024-06-07 11:09:49 -0700

    New files

---
+++ car.py
@@ -0,0 +1,6 @@
+import pygame
+import math
+
+class Car:
+    def __init__(self, width, height):
+        
---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+import neat
+from drawing import Drawing
+from car import Car
+
+def main():
+
+    p.run(run_simulation, 1000, game_map)
+
+if __name__ == "__main__":
+    main()
